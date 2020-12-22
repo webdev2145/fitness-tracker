c2470cdd96c9ee0ab415f760496416c149c61444
Oneil Pinnock <web.dev2145@gmail.com>
2020-12-22 12:24:06 -0500

    addd username and prject to sheety endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from credentials import *
-from datetime import date
+from datetime import datetime
 
 
 GENDER = "male"
@@ -10,9 +10,6 @@ AGE = 30
 
 #TODO 1: SET API ID and KEY
 #Added from credentials file
-
-
-SHEETY_GOOGLE_SHEETS_ENDPOINT = f"https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECT}/sheetNam"
 
 #TODO 2: Use Nutronix NL for exercise API Docs to print stats for a plain text input
 NUTRONIX_API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
@@ -37,4 +34,27 @@ response = requests.post(url=NUTRONIX_API_ENDPOINT, headers=headers, json=reques
 result = response.json()
 print(result)
 
+##SHEETY SETUP
+SHEETY_GOOGLE_SHEETS_ENDPOINT = f"https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECT}/{SHEETY_NAME}"
+
+today_date = datetime.now().strftime("%d%m%Y")
+today_time = datetime.now().strftime("%X")
+
+workout_data = result["exercises"]
+
+for workout in workout_data:
+    sheety_parameters = {
+        "workouts": {
+            "date": today_date,
+            "time": today_time,
+            "exercise": workout["name"].title(),
+            "duration": workout["duration_min"],
+            "calories": workout["nf_calories"]
+        }
+
+    }
+
+sheety_response = requests.post(url=SHEETY_GOOGLE_SHEETS_ENDPOINT, json=sheety_parameters)
+
+print(sheety_response.text)
 
